afbc40fa34fe725349ecd7bfe55a1ded5cde9f31
Serena Cheng <serena.ch1999@gmail.com>
2020-04-25 16:11:13 -0400

    Display all pets.

--- index.php
+++ index.php
@@ -15,11 +15,11 @@
     </head>
     
     <body>
-  		<h1>List of Actions</h1>
+  		<h1>Pet Store DBMS</h1>
   		
   		<h2>Pets</h2>
     	<ul>
-    		<li><a href="pet.php">List all pets</a></li>
+    		<li><a href="pet.php">Display all pets</a></li>
     	</ul>
     </body>
 </html>
