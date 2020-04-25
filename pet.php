6310db8b3d7ec354b89fefcc13d9365a36d4b07d
Serena Cheng <serena.ch1999@gmail.com>
2020-04-25 15:45:33 -0400

    Display all pets.

--- pet.php
+++ pet.php
@@ -1,64 +1,65 @@
 <html>
-<head>
-<style>
-table, th, td {
-  border: 1px solid black;
-  padding: 3px;
-}
+	<head>
+		<style>
+            table, td {
+            	border: 1px solid black;
+            	padding: 3px;
+            }
+            
+            td {
+                border: 1px solid black;
+                padding: 7px;
+                text-align: right;
+            }
+        </style>
+	</head>
+	<body>
 
-th, td {
-  padding: 10px;
-}
-</style>
-</head>
-<body>
-
-<?php
-
-require_once('connection.php');
-
-setlocale(LC_MONETARY, 'en_US');
-
-// Retrieve list of employees
-$stmt = $conn->prepare('select p_id, pet_name, birthdate, price, available, store from Pet order by p_id;');
-$stmt->execute();
-
-echo "<table>";
-echo "<thead><tr>
-    <th>ID</th>
-    <th>Pet name</th>
-    <th>Birthdate</th>
-    <th>Price</th>
-    <th>Availability</th>
-    <th>Store</th>
-    </tr></thead>";
-echo "<tbody>";
-
-while ($row = $stmt->fetch()) {
-    echo "<tr><td>$row[p_id]</td>";
+        <?php
+        require_once ('connection.php');
         
-    if ($row["pet_name"] == null || $row["pet_name"] == "") {
-       echo '<td>No name</td>'; 
-    } else { 
-       echo "<td>" . $row["pet_name"] . "</td>";
-    }
-    
-    if ($row["birthdate"] == null) {
-        echo '<td>N/A</td>';
-    } else {
-        echo "<td>" . $row["birthdate"] . "</td>";
-    }
-    
-    echo "<td>" . money_format("%.2n", $row["price"]) . "</td>
-    <td>$row[available]</td>
-    <td>$row[store]</td>
-    </tr>";
-}
+        setlocale(LC_MONETARY, 'en_US');
+        
+        // Retrieve list of employees
+        $stmt = $conn->prepare('select p_id, pet_name, birthdate, price, available, store from Pet order by p_id;');
+        $stmt->execute();
+        
+        echo "<table>";
+        echo "<thead><tr>
+            <th>ID</th>
+            <th>Pet name</th>
+            <th>Birthdate</th>
+            <th>Price</th>
+            <th>Availability</th>
+            <th>Store</th>
+            </tr></thead>";
+        echo "<tbody>";
+        
+        while ($row = $stmt->fetch()) {
+            echo "<tr><td>$row[p_id]</td>";
+        
+            if ($row["pet_name"] == null || $row["pet_name"] == "") {
+                echo '<td>No name</td>';
+            } else {
+                echo "<td>" . $row["pet_name"] . "</td>";
+            }
+        
+            if ($row["birthdate"] == null) {
+                echo '<td>N/A</td>';
+            } else {
+                echo "<td>" . $row["birthdate"] . "</td>";
+            }
+        
+            echo "<td>" . money_format("%.2n", $row["price"]) . "</td>
+            <td>$row[available]</td>
+            <td>$row[store]</td>
+            </tr>";
+        }
+        
+        echo "</tbody>";
+        echo "</table>";
+        
+        ?>
 
-echo "</tbody>";
-echo "</table>";
-
-?>
-
-</body>
+	</body>
 </html>
