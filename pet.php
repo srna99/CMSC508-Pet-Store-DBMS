930ccf34d8e207b66bdb98157f553e239588efe1
Serena Cheng <serena.ch1999@gmail.com>
2020-04-25 14:05:02 -0400

    Display all pets.

--- pet.php
+++ pet.php
@@ -5,15 +5,16 @@ require_once('connection.php');
 setlocale(LC_MONETARY, 'en_US');
 
 // Retrieve list of employees
-$stmt = $conn->prepare('select p_id, pet_name, birthdate, price, available, store from Pet order by p_id;');
+$stmt = $conn->prepare('select p_id, ifnull(pet_name, "No name"), ifnull(birthdate, "N/A"), price, available, store from Pet order by p_id;');
 $stmt->execute();
 
 echo "<table style='border: solid 1px black;'>";
 echo "<thead><tr><th>ID</th><th>Pet name</th><th>Birthdate</th><th>Price</th><th>Availability</th><th>Store</th></tr></thead>";
 echo "<tbody>";
 
-$row = $stmt->fetch();
-echo "<tr><td>$row[p_id]</td><td>$row[pet_name]</td><td>$row[birthdate]</td><td>" . money_format("%.2n", $row["price"]) . "</td><td>$row[available]</td><td>$row[store]</td></tr>";
+while ($row = $stmt->fetch()) {
+    echo "<tr><td>$row[p_id]</td><td>$row[pet_name]</td><td>$row[birthdate]</td><td>" . money_format("%.2n", $row["price"]) . "</td><td>$row[available]</td><td>$row[store]</td></tr>";
+}
 
 echo "</tbody>";
 echo "</table>";
