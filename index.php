4e9971a2fbbee121c18d7c3071b8c953dae521a6
Serena Cheng <serena.ch1999@gmail.com>
2020-04-25 16:03:16 -0400

    Display all pets.

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <html>
-<head>
-<title>CMSC 508 Pet Store DBMS</title>
-</head>
-<body>
-	<ul>
-		<li><a href="pet.php">List all pets</a></li>
-	</ul>
-</body>
+    <head>
+    	<title>CMSC 508 Pet Store DBMS</title>
+    </head>
+    <body>
+  		<h1>List of Actions</h1>
+  		
+  		<h2>Pets</h2>
+    	<ul>
+    		<li><a href="pet.php">List all pets</a></li>
+    	</ul>
+    </body>
 </html>
