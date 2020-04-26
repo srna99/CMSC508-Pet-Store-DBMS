907055b021ba28a5ef31c538bc1fea30fc52c6aa
Serena Cheng <serena.ch1999@gmail.com>
2020-04-25 22:12:52 -0400

    Edit animal.

--- editAnimal.php
+++ editAnimal.php
@@ -35,7 +35,7 @@
         session_start();
         
         // first page
-        if (!isset($_GET['employee_id']) && $_SERVER['REQUEST_METHOD'] != 'POST') {
+        if (!isset($_GET['classification']) && $_SERVER['REQUEST_METHOD'] != 'POST') {
             
             $stmt = $conn->prepare('select classification from Animal order by classification;');
             $stmt->execute();
