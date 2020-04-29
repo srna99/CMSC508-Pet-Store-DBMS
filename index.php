9555cc42c4387a303c7da28a7c6fa1a1b0d09c9a
Serena Cheng <serena.ch1999@gmail.com>
2020-04-28 21:53:31 -0400

    Display all stockers.

--- index.php
+++ index.php
@@ -71,6 +71,7 @@
     		<li><a href="employee.php">Display All Employees</a></li>
     		<li><a href="cashier.php">Display All Cashiers</a></li>
     		<li><a href="groomer.php">Display All Groomers</a></li>
+    		<li><a href="stocker.php">Display All Stockers</a></li>
     		<li><a href="addEmployee.php">Add New Employee</a></li>
     		<li><a href="editEmployee.php">Edit An Employee</a></li>
     		<li><a href="deleteEmployee.php">Delete An Employee</a></li>
---
+++ stocker.php
@@ -0,0 +1,65 @@
+<html>
+	<head>
+		<style>
+            table, th, td {
+            	border: 1px solid black;
+            	border-collapse: collapse;
+            }
+            
+            table {
+                margin-bottom: 30px;
+                margin-left: 0.5em;
+            }
+            
+            th, td {
+                padding-left: 10px;
+                padding-right: 10px;
+                padding-top: 5px;
+                padding-bottom: 5px;
+            }
+            
+            h1 {
+                margin-top: 0.8em;
+                margin-left: 0.5em;
+            }
+        </style>
+	</head>
+	
+	<body>
+		<h1>List of All Cashiers</h1>
+	
+        <?php
+        require_once ('connection.php');
+        
+        // get all stockers
+        $stmt = $conn->prepare('select e_id, concat(first_name, " ", last_name) as name, employment_status, product_to_stock from Stocker left join Employee using(e_id) order by name;');
+        $stmt->execute();
+        
+        // make table
+        echo "<table>";
+        // row headings
+        echo "<thead><tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Employment status</th>
+            <th>Product to stock</th>
+            </tr></thead>";
+        echo "<tbody>";
+        
+        // info from query
+        while ($row = $stmt->fetch()) {
+            echo "<tr><td>$row[e_id]</td>
+            <td>$row[name]</td>
+            <td>$row[employment_status]</td>
+            <td>$row[product_to_stock]</td></tr>";
+        }
+        
+        echo "</tbody>";
+        echo "</table>";
+        
+        ?>
+        
+        <!-- back to index link -->
+		<a href="index.php">Back to index</a>
+	</body>
+</html>
