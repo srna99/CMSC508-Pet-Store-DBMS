f890488ab707af8e5f515a8a8f4ca9db6dc6da3c
Serena Cheng <serena.ch1999@gmail.com>
2020-04-28 21:40:35 -0400

    Fix formatting.

--- index.php
+++ index.php
@@ -28,38 +28,38 @@
     		<li><a href="animal.php">Display All Types of Animals</a></li>
     		<li><a href="addAnimal.php">Add New Type of Animal</a></li>
     		<li><a href="editAnimal.php">Edit Diet Type of Animal</a></li>
-    		<li><a href="deleteAnimal.php">Delete a Type of Animal</a></li>
+    		<li><a href="deleteAnimal.php">Delete A Type of Animal</a></li>
     		<br>
     		<li><a href="pet.php">Display All Pets</a></li>
     		<li><a href="addPet.php">Add New Pet</a></li>
     		<li><a href="editPet.php">Edit Pet</a></li>
-    		<li><a href="deletePet.php">Delete a Pet</a></li>
+    		<li><a href="deletePet.php">Delete A Pet</a></li>
     	</ul>
     	
     	<h2>Stock</h2>
     	<ul>
 			<li><a href="accessory.php">Display All Accessories</a></li>
 			<li><a href="addAccessory.php">Add New Accessory</a></li>
-			<li><a href="editAccessory.php">Edit an Accessory</a></li>
-			<li><a href="deleteAccessory.php">Delete an Accessory</a></li>
+			<li><a href="editAccessory.php">Edit An Accessory</a></li>
+			<li><a href="deleteAccessory.php">Delete An Accessory</a></li>
 			<br>
 			<li><a href="bedding.php">Display All Bedding</a></li>
 			<li><a href="addBedding.php">Add New Bedding</a></li>
-			<li><a href="editBedding.php">Edit a Bedding</a></li>
-			<li><a href="deleteBedding.php">Delete a Bedding</a></li>
+			<li><a href="editBedding.php">Edit A Bedding</a></li>
+			<li><a href="deleteBedding.php">Delete A Bedding</a></li>
 			<br>
 			<li><a href="food.php">Display All Food</a></li>
 			<li><a href="addFood.php">Add New Food</a></li>
-			<li><a href="editFood.php">Edit a Food</a></li>
-			<li><a href="deleteFood.php">Delete a Food</a></li>
+			<li><a href="editFood.php">Edit A Food</a></li>
+			<li><a href="deleteFood.php">Delete A Food</a></li>
 		</ul>
 		
     	<h2>Customers</h2>
     	<ul>
     		<li><a href="customer.php">Display All Customers</a></li>
     		<li><a href="addCustomer.php">Add New Customer</a></li>
-    		<li><a href="editCustomer.php">Edit a Customer</a></li>
-    		<li><a href="deleteCustomer.php">Delete a Customer</a></li>
+    		<li><a href="editCustomer.php">Edit A Customer</a></li>
+    		<li><a href="deleteCustomer.php">Delete A Customer</a></li>
     		<br>
     		<li><a href="customerTransactions.php">Display All Customers' Transactions</a></li>
     		<li><a href="addCustomerTransaction.php">Add New Transaction</a></li>
