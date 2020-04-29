32a8783e6fb5cbb5d67b8c8eb16c2eed76b854c8
Serena Cheng <serena.ch1999@gmail.com>
2020-04-28 22:00:29 -0400

    Change formatting.

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     	<title>CMSC 508 Pet Store DBMS</title>
     	
     	<style>
-    	    h1, h2, ul {
-    	        margin-left: auto;
-    	        margin-right:auto;
-    	    }
+    	    .content {
+              max-width: 500px;
+              margin: auto;
+            }
     	    
     	    h1 {
                 text-align: center;
@@ -24,66 +24,66 @@
     </head>
     
     <body>
-    
-  		<h1>Pet Store DBMS</h1>
-  		
-  		<h2>Pets</h2>
-    	<ul>
-    		<li><a href="animal.php">Display All Types of Animals</a></li>
-    		<li><a href="addAnimal.php">Add New Type of Animal</a></li>
-    		<li><a href="editAnimal.php">Edit Diet Type of Animal</a></li>
-    		<li><a href="deleteAnimal.php">Delete A Type of Animal</a></li>
-    		<br>
-    		<li><a href="pet.php">Display All Pets</a></li>
-    		<li><a href="addPet.php">Add New Pet</a></li>
-    		<li><a href="editPet.php">Edit Pet</a></li>
-    		<li><a href="deletePet.php">Delete A Pet</a></li>
-    	</ul>
-    	
-    	<h2>Stock</h2>
-    	<ul>
-			<li><a href="accessory.php">Display All Accessories</a></li>
-			<li><a href="addAccessory.php">Add New Accessory</a></li>
-			<li><a href="editAccessory.php">Edit An Accessory</a></li>
-			<li><a href="deleteAccessory.php">Delete An Accessory</a></li>
-			<br>
-			<li><a href="bedding.php">Display All Bedding</a></li>
-			<li><a href="addBedding.php">Add New Bedding</a></li>
-			<li><a href="editBedding.php">Edit A Bedding</a></li>
-			<li><a href="deleteBedding.php">Delete A Bedding</a></li>
-			<br>
-			<li><a href="food.php">Display All Food</a></li>
-			<li><a href="addFood.php">Add New Food</a></li>
-			<li><a href="editFood.php">Edit A Food</a></li>
-			<li><a href="deleteFood.php">Delete A Food</a></li>
-		</ul>
-		
-    	<h2>Customers</h2>
-    	<ul>
-    		<li><a href="customer.php">Display All Customers</a></li>
-    		<li><a href="addCustomer.php">Add New Customer</a></li>
-    		<li><a href="editCustomer.php">Edit A Customer</a></li>
-    		<li><a href="deleteCustomer.php">Delete A Customer</a></li>
-    		<br>
-    		<li><a href="customerTransactions.php">Display All Customers' Transactions</a></li>
-    		<li><a href="addCustomerTransaction.php">Add New Transaction</a></li>
-    		<li><a href="customerVisited.php">Display All Stores Visited By A Customer</a></li>
-    	</ul>
-    	
-    	<h2>Personnel and Related Matters</h2>
-    	<ul>
-    		<li><a href="employee.php">Display All Employees</a></li>
-    		<li><a href="cashier.php">Display All Cashiers</a></li>
-    		<li><a href="groomer.php">Display All Groomers</a></li>
-    		<li><a href="stocker.php">Display All Stockers</a></li>
-    		<li><a href="addEmployee.php">Add New Employee</a></li>
-    		<li><a href="editEmployee.php">Edit An Employee</a></li>
-    		<li><a href="deleteEmployee.php">Delete An Employee</a></li>
-    	</ul>
-    	
-    	<h2>Stores and Businesses</h2>
-    	<ul id="last">
-    	</ul>
-    	
+    	<div class="content">
+      		<h1>Pet Store DBMS</h1>
+      		
+      		<h2>Pets</h2>
+        	<ul>
+        		<li><a href="animal.php">Display All Types of Animals</a></li>
+        		<li><a href="addAnimal.php">Add New Type of Animal</a></li>
+        		<li><a href="editAnimal.php">Edit Diet Type of Animal</a></li>
+        		<li><a href="deleteAnimal.php">Delete A Type of Animal</a></li>
+        		<br>
+        		<li><a href="pet.php">Display All Pets</a></li>
+        		<li><a href="addPet.php">Add New Pet</a></li>
+        		<li><a href="editPet.php">Edit Pet</a></li>
+        		<li><a href="deletePet.php">Delete A Pet</a></li>
+        	</ul>
+        	
+        	<h2>Stock</h2>
+        	<ul>
+    			<li><a href="accessory.php">Display All Accessories</a></li>
+    			<li><a href="addAccessory.php">Add New Accessory</a></li>
+    			<li><a href="editAccessory.php">Edit An Accessory</a></li>
+    			<li><a href="deleteAccessory.php">Delete An Accessory</a></li>
+    			<br>
+    			<li><a href="bedding.php">Display All Bedding</a></li>
+    			<li><a href="addBedding.php">Add New Bedding</a></li>
+    			<li><a href="editBedding.php">Edit A Bedding</a></li>
+    			<li><a href="deleteBedding.php">Delete A Bedding</a></li>
+    			<br>
+    			<li><a href="food.php">Display All Food</a></li>
+    			<li><a href="addFood.php">Add New Food</a></li>
+    			<li><a href="editFood.php">Edit A Food</a></li>
+    			<li><a href="deleteFood.php">Delete A Food</a></li>
+    		</ul>
+    		
+        	<h2>Customers</h2>
+        	<ul>
+        		<li><a href="customer.php">Display All Customers</a></li>
+        		<li><a href="addCustomer.php">Add New Customer</a></li>
+        		<li><a href="editCustomer.php">Edit A Customer</a></li>
+        		<li><a href="deleteCustomer.php">Delete A Customer</a></li>
+        		<br>
+        		<li><a href="customerTransactions.php">Display All Customers' Transactions</a></li>
+        		<li><a href="addCustomerTransaction.php">Add New Transaction</a></li>
+        		<li><a href="customerVisited.php">Display All Stores Visited By A Customer</a></li>
+        	</ul>
+        	
+        	<h2>Personnel and Related Matters</h2>
+        	<ul>
+        		<li><a href="employee.php">Display All Employees</a></li>
+        		<li><a href="cashier.php">Display All Cashiers</a></li>
+        		<li><a href="groomer.php">Display All Groomers</a></li>
+        		<li><a href="stocker.php">Display All Stockers</a></li>
+        		<li><a href="addEmployee.php">Add New Employee</a></li>
+        		<li><a href="editEmployee.php">Edit An Employee</a></li>
+        		<li><a href="deleteEmployee.php">Delete An Employee</a></li>
+        	</ul>
+        	
+        	<h2>Stores and Businesses</h2>
+        	<ul id="last">
+        	</ul>
+    	</div>
     </body>
 </html>
