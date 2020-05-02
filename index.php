74bf316799812e43a21e2dcaf4e474a86570186d
Serena Cheng <serena.ch1999@gmail.com>
2020-05-02 18:00:52 -0400

    Increase salaries.

--- index.php
+++ index.php
@@ -142,6 +142,8 @@
                 	<br><a href="trainer.php">Display All Trainers</a>
                 	<br><a href="addEmployee.php">Add New Employee</a>
                 	<br><a href="editEmployee.php">Edit An Employee</a>
+                	<br><a href="increaseSalaryManager.php">Increase Salary of Managers</a>
+                	<br><a href="increaseSalarySalary.php">Increase Salary of Employees By Store</a>
                 	<br><a href="deleteEmployee.php">Delete An Employee</a>
                 	<br>
                 	<br><a href="clipper.php">Display All Clippers</a>
